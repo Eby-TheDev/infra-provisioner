8e6bb849b16e99e4b7613b8654a3ac7f3b655079
Eby <amalsibik@gmail.com>
2020-03-14 12:39:13 +0530

    First infra

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "google" {
+  version = "3.5.0"
+
+  credentials = file("terraform-project-271105-b27796352649.json")
+
+  project = "terraform-project-271105"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
